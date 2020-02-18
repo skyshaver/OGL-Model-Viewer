e270d200370009c4b46e66fbd5868ffe25152164
Sky Shaver <skshaver@amd.com>
2020-02-18 13:07:11 -0500

    added FreeType lib through vcpkg and updated main to render text, all text rendering functions are in main, need to factor out to a class, text is rendering properly as is though

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 #include <iostream>
+#include <map>
+#include <utility>
+#include <string>
 
 #include "Shader.h"
 #include "Camera.h"
@@ -25,6 +31,14 @@ float deltaTime = 0.f;
 float lastFrame = 0.f;
 // light box pos
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+// text 
+struct Character {
+	GLuint TextureID;		// id handle of the glyph texture
+	glm::ivec2 Size;		// size of glyph
+	glm::ivec2 Bearing;		// offset from baseline to left/top of glyph
+	GLuint Advance;			// offset to advance to next glyph
+};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -113,6 +127,50 @@ void printMaxVertexAttribs()
 	std::cout << "Max number of vertex attribs: " << nrAttributes << std::endl;
 }
 
+void RenderText(Shader& s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color, std::map<GLchar, Character> Characters, GLuint VAO, GLuint VBO)
+{
+	// Activate corresponding render state	
+	s.use();
+	glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(VAO);
+
+	// Iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+
+		GLfloat xpos = x + ch.Bearing.x * scale;
+		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		GLfloat w = ch.Size.x * scale;
+		GLfloat h = ch.Size.y * scale;
+		// Update VBO for each character
+		GLfloat vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos,     ypos,       0.0, 1.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+			{ xpos + w, ypos + h,   1.0, 0.0 }
+		};
+		// Render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// Update content of VBO memory
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// Render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6)* scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 
 int main() 
 {
@@ -141,6 +199,57 @@ int main()
 		std::cout << "failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	// init FreeType and load font
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+	{
+		std::cout << "ERROR::FREETYPE: could not init FreeType Library" << std::endl;
+	}
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+	{
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+	}
+	// define font size to extract from face
+	FT_Set_Pixel_Sizes(face, 0, 48);
+	// create map of character glyph textures
+	std::map<GLchar, Character> Characters;
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte alignment restriction
+
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		// load character glyph
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// Generate texture
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer);
+		// set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// store character in map
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(std::pair<GLchar, Character>(c, character));
+	}
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	
+	// end text setup
+
 
 	float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -188,8 +297,12 @@ int main()
 
 	Shader lightingShader("shaders/shader.vert", "shaders/shader.frag");
 	Shader lampShader("shaders/light_shader.vert", "shaders/light_shader.frag");
+	Shader textShader("shaders/text_shader.vert", "shaders/text_shader.frag");
 	// enable depth testing
 	glEnable(GL_DEPTH_TEST);
+	// enable blending for text rendering
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// buffer for boxes
 	unsigned int VBO, cubeVAO;// vertex buffer object, vertex arrray object, element buffer object
@@ -214,11 +327,29 @@ int main()
 	// we only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; 
 	// the VBO's data already contains all we need (it's already bound, but we do it again for educational purposes)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glad_glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// buffer for text
+	GLuint textVAO, textVBO;
+	glGenVertexArrays(1, &textVAO);
+	glGenBuffers(1, &textVBO);
+	glBindVertexArray(textVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, nullptr, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	int countedFrames = 0;
+	int fps = 0;
 	while (!glfwWindowShouldClose(window))
 	{
+		countedFrames++;
+		static float startTime = glfwGetTime();
+		float elapsedTime = glfwGetTime() - startTime;
+		fps = countedFrames / elapsedTime;
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
@@ -229,7 +360,10 @@ int main()
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// activate shaders
+
+		
+
+		// activate lighting shaders
 		lightingShader.use();
 		glm::vec3 objectColor(1.f, 0.5f, 0.31f);
 		glm::vec3 lightColor(1.f, 1.f, 1.f);
@@ -249,7 +383,7 @@ int main()
 		glBindVertexArray(cubeVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		
-		// render lamp object
+		// activate lamp shader
 		lampShader.use();
 		lampShader.setMat4("projection", projection);
 		lampShader.setMat4("view", view);
@@ -260,6 +394,14 @@ int main()
 		// render lamp object
 		glBindVertexArray(lightVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// activate text shader
+		textShader.use();
+		glm::mat4 textProjection = glm::ortho(0.f, 800.f, 0.f, 600.f);
+		textShader.setMat4("textProjection", textProjection);
+		RenderText(textShader, "FPS: " + std::to_string(fps), 25.f, 25.f, 0.25f, glm::vec3(0.5f, 0.8f, 0.2f), Characters, textVAO, textVBO);
+		RenderText(textShader, "Camera xpos: " + std::to_string(camera.Position.x) + 
+			 " ypos: " + std::to_string(camera.Position.y), 25.f, 50.f, 0.25f, glm::vec3(0.5f, 0.8f, 0.2f), Characters, textVAO, textVBO);
 		
 		//____________________________
 		glfwSwapBuffers(window);
